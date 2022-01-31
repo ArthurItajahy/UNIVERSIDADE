f962e7021f6d2ca3bfbba902ccc87a6a1c0958a8
Paulo <paulo-ricardo_23@hotmail.com>
2022-01-31 13:47:00 -0300

    removendo a pasta bin

--- Aplication/src/Aplication/Main.java
+++ Aplication/src/Aplication/Main.java
@@ -3,10 +3,7 @@ package Aplication;
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello2");
-		Professor p = new Professor();
-		p.setName("Paulo");
-		p.toString();
+		
 	}
 
 }
